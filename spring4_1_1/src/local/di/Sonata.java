2838486a7db6dfcdbb34ddf7625684001e32d742
wpwjd169 <wpwjd169@naver.com>
2021-05-14 10:55:56 +0900

    스프링 생성자 객체 주입법 코드 실습

---
+++ spring4_1_1/src/local/di/Sonata.java
@@ -0,0 +1,23 @@
+package local.di;
+
+//@Scope("singletone")
+public class Sonata {
+	private String carColor = null;
+	private int wheelNum = 0;
+	private int speed = 0;
+	public Sonata() {}
+	public Sonata(String carColor, int wheelNum) {
+		this.carColor=carColor;
+		this.wheelNum=wheelNum;
+	}
+	public Sonata(String carColor, int wheelNum, int speed) {
+		this.carColor=carColor;
+		this.wheelNum=wheelNum;
+		this.speed=speed;
+	}
+	
+	@Override
+	public String toString() {
+		return "자동차 정보 출력하기";
+	}
+}
